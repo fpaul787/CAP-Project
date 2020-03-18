7cbe734ccc855168eb9ff843012c379f1d30582f
Manny Castaneda <Ecast221@gmail.com>
2020-03-17 22:50:33 -0400

    React App

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const bodyParser = require('body-parser');
+
+require ('dotenv/config');
+
+// Must use in tandem with bodyParser
+app.use(bodyParser.json());
+
+//Import routes
+const postsRoute = require('./routes/posts');
+
+// Link /posts to postsRoutes (which links to './routes/posts' where it you have app.get('/')
+app.use('/posts', postsRoute);
+
+
+// Middleware goes here, functions go here that happen when
+// somethings pings a route.
+//app.use('/posts', () => {
+//   console.log('this function runs whenever /posts hits.')
+//});
+
+
+
+//Routes
+// Type new route in a '/routegoeshere'
+app.get('/', (req, res) => {
+    res.send("We are home!");
+});
+
+
+
+//Connect to DB
+// To access '.env' file's DB_CONNECTION, simply require ('dotenv/config'), and address it as 'process.env.DB_CONNECTION'
+mongoose.connect(process.env.DB_CONNECTION,  
+{ useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log("Connected to database");
+});
+
+// Listen to server
+app.listen(3000);
