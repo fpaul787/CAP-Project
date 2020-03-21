a3eea8b0c9e093d8dc8ba3a8082830d7978bc1b8
Manny Castaneda <Ecast221@gmail.com>
2020-03-21 12:28:11 -0400

    Checkboxes

--- client/src/components/Search/searchforclasses.js
+++ client/src/components/Search/searchforclasses.js
@@ -151,29 +151,29 @@ class SearchForClasses extends Component {
         return (
             <div>
                 <div style={{padding: '30px', position: 'relative'}}>
-                    <FlowChartBox action={this.handleClick.bind(this,0)} top={44} left={41} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,1)}  top={44} left={175} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,2)}  top={44} left={308} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,3)}  top={44} left={690} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,4)} top={44} left={840} width={116} height={84}></FlowChartBox>
-                    {/* <FlowChartBox action={this.handleClick.bind(this,2)}  t={44} l={308} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,3)}  t={44} l={690} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,4)} t={44} l={840} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,5)}  t={44} l={974} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,6)}  t={177} l={41} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,7)}  t={177} l={175} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,8)}  t={188} l={328} width={101} height={60}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,9)}  t={188} l={469} width={93} height={58}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,10)}  t={177} l={692} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,11)}  t={177} l={974} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,12)}  t={310} l={175} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,13)}  t={310} l={424} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,14)}  t={310} l={692} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,15)}  t={310} l={974} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,16)}  t={475} l={41} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,17)}  t={426} l={692} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,18)}  t={475} l={424} width={116} height={84}></FlowChartBox>
-                    <FlowChartBox action={this.handleClick.bind(this,19)} t={475} l={974} width={116} height={84}></FlowChartBox> */}
+                    <FlowChartBox action={this.handleClick.bind(this,0)}   top={44}  left={41} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,1)}   top={44}  left={175} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,2)}   top={44}  left={308} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,3)}   top={44}  left={690} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,4)}   top={44}  left={840} width={116} height={84}></FlowChartBox>
+                     <FlowChartBox action={this.handleClick.bind(this,2)}  top={44}  left={308} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,3)}   top={44}  left={690} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,4)}   top={44}  left={840} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,5)}   top={44}  left={974} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,6)}   top={177} left={41} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,7)}   top={177} left={175} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,8)}   top={188} left={328} width={101} height={60}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,9)}   top={188} left={469} width={93} height={58}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,10)}  top={177} left={692} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,11)}  top={177} left={974} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,12)}  top={310} left={175} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,13)}  top={310} left={424} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,14)}  top={310} left={692} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,15)}  top={310} left={974} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,16)}  top={475} left={41} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,17)}  top={426} left={692} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,18)}  top={475} left={424} width={116} height={84}></FlowChartBox>
+                    <FlowChartBox action={this.handleClick.bind(this,19)}  top={475} left={974} width={116} height={84}></FlowChartBox> 
                     <img src={ require('../../img/flowchart.png') } />
                 </div>
                 
--- client/src/components/Search/searchresults.js
+++ client/src/components/Search/searchresults.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
-import { Button } from 'reactstrap';
+import {Card} from 'react-bootstrap'
+import {Button} from 'reactstrap'
+
+import '../styles/flowstyles.css';
+
 
 const courseNames = ["Calculus I", "Natural Science I", "Natural Science II", "Programming I", "Introduction to Computing",
                         "Professional and Technical Writing","Calculus II", "Physics I",  "Discrete Math",  "Discrete Structures", "Programming II",
                         "Technology in the Global Arena", "Physics II", "Computer Architecture", "Data Structures", "Software Engineering I",
                         "Statistics", "Operating Systems", "Systems Programming", "Senior Project"]
+
+const Checkbox = props => (
+                            <input type="checkbox" {...props} />
+                          );
 
 class SearchResults extends Component {
     
@@ -13,7 +21,9 @@ class SearchResults extends Component {
 
         this.state = {
             'courses': [],
-            'query': []
+            'query': [],
+            'chosenClasses': [],
+            courseCount: 0
         }
     }
 
@@ -40,17 +50,82 @@ class SearchResults extends Component {
         .then(results => this.setState({'courses': results.data}));
     
     }
+
+    handleCheckboxChange(course){
+        // remove if exists
+        if (this.state.chosenClasses.includes(course._id)){
+            var index = this.state.chosenClasses.indexOf(course._id);
+            if (index > -1) {
+                this.state.chosenClasses.splice(index, 1);
+             }
+             this.setState({courseCount: this.state.courseCount-1});
+        } else {
+            this.state.chosenClasses.push(course._id);
+            this.setState({courseCount: this.state.courseCount+1});
+        }
+
+        console.log(this.state.chosenClasses);
+
+    }
     
     render() {
         return (
-            <div style={{padding: '30px'}}>
+            <div>
+            <div style={{border: '1px solid grey', padding: '30px', overflowY: 'scroll', height: '720px', padding: '20px', marginLeft: '50px', marginRight: '50px', marginTop: '20px'}}>,
                 {this.state.courses.map(function(item, index) {
                     for (let i = 0; i < 20; i++){
                         if (item.name === this.state.query[i]){
-                            return <div style={{float: 'left', width: '30%', padding: '20px', margin: '10px', backgroundColor: '#ddd', border: '1px solid black'}}>{item.name} {item.hour}</div>
-                        }
+                            return <Card
+                            border="secondary"
+                    
+                            style={{ width: '30%', float: 'left', margin: '15px'}}
+                          >
+                            <Card.Header><Card.Title>{item.name}</Card.Title></Card.Header>
+                            <Card.Body>
+                              
+                              <Card.Text>
+                                <p style={{textIndent: '30px', float: 'left'}}>{item.hour}</p>
+                                <label>
+                                    <Checkbox style={{float: 'right', position: 'absolute', right: '0', marginRight: '30px', width: '25px', height: '25px'}}
+                                        
+                                        onClick={this.handleCheckboxChange.bind(this, item)}
+                                    />
+                                    
+                                    </label>
+                              </Card.Text>
+                              
+                              
+                            </Card.Body>
+                            
+                          </Card>
+                         }
                     }
             }, this)}
+            
+             </div>
+             
+             <span style={{float: 'right', marginTop: '20px', marginRight: '50px'}}>
+             <strong style={{marginRight: '50px'}}>{this.state.courseCount} Classes Selected.</strong>
+                <Button 
+                    onClick={() => console.log(this.state.selected)}
+                    color="primary" 
+                    style={
+                        {
+                        paddingLeft: '60px', 
+                        paddingRight: '60px'}}>
+                    Add to Cart
+                </Button>
+                <Button 
+                    onClick={() => console.log(this.state.selected)}
+                    color="success" 
+                    style={
+                        {
+                            paddingLeft: '60px', 
+                            paddingRight: '60px',
+                            marginLeft: '15px'}}>
+                    Enroll
+                </Button>
+            </span>
              </div>
         );
         
